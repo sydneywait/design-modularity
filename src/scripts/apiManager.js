f5608f09cbf0f1bc4f588aea055e7977b1f68e2d
sydneywait <sydneywait@gmail.com>
2019-02-05 14:54:41 -0500

    used CSS to flex boxes, aligned button on bottom right, fixed comma in leading string

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -18,7 +18,7 @@ fetch("https://data.nashville.gov/resource/xbru-cfzi.json")
             let featureString = buildFeatureString(featureArray)
             let parkObjectToAdd = makeParkObject(element, featureString)
             let cardToPrint = makeCard(parkObjectToAdd)
-            console.log("card to print", cardToPrint)
+            // console.log("card to print", cardToPrint)
             renderToDom("#park-cards", cardToPrint)
             parkObjectArray.push(parkObjectToAdd)
             // console.log(parkObjectArray)
@@ -45,23 +45,29 @@ const makeParkObject = (object, features) => {
 
 ////Function to get amenities in the features array and turn it into a string
 
-const test = ["hiking_trails", "nature_center", "restrooms_available"]
+
 
 let buildFeatureString = (array) => {
-    console.log("I am inside the buildFeatureString function")
+    // console.log("I am inside the buildFeatureString function")
     let featureString = ""
     array.forEach(thing => {
-        console.log("I am inside the for loop")
+        // console.log("I am inside the for loop")
 
         let thing2 = thing.split("_").join(" ")
         featureString =`${featureString}, ${thing2}`
-        console.log("this is the feature string", featureString)
-    })
 
+
+    })
+    console.log("this is the feature string", featureString)
+    //remove the leading comma
+    featureString = featureString.replace(", ", "")
     return featureString;
 }
-
+// const test = ["hiking_trails", "nature_center", "restrooms_available"]
 // const testString = buildFeatureString(test);
 // console.log("this is the test String", testString)
+
+
+
 
 
--- src/scripts/makeCard.js
+++ src/scripts/makeCard.js
@@ -1,11 +1,11 @@
 const makeCard = (parkObject) => {
 
-return `<div class="card" style="width: 18rem;">
+return `<div class="card">
     <div class="card-body">
     <h4 class="card-title">${parkObject.name}</h4>
     <h5 class="card-title">${parkObject.address}</h5>
     <p class="card-text">This park contains ${parkObject.size} of pristinely maintained park land.  It also includes the following amenities: ${parkObject.features}</p>
-    <button type="button" class="btn btn-primary">Save</button>
+    <button type="button" class="btn btn-save">Save</button>
   </div>
 </div>`
 
